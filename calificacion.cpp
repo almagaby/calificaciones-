86f04abdaa9a1ececed1bf37ad7f9d7fd0dae4d7
almagaby <82476125+almagaby@users.noreply.github.com>
2024-11-13 21:11:03 -0600

    Add files via upload

---
+++ calificacion.cpp
@@ -0,0 +1,18 @@
+
+
+using namespace std;
+
+int main (){
+	double calificacion;
+	cout <<"ingresa la calificacion del alimno";
+	cin>> calificacion;
+	
+	
+	if (calificacion >=8){
+		cout <<"fwlicidades estas aprovado";
+		
+	} 
+	
+
+return 0;
+}
